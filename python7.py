d427ed82785385e6da3c34ffdd0b7ab2cb4aab5d
suryakiran0666 <jonnavithulasuryakiran1@gmail.com>
2026-09-15 11:00:23 +0530

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,8 @@
+#area=l*b
+#perimeter=2(l+b)
+length=int(input("enter the value"))
+breadth=int(input("enter the value"))
+area=length*breadth
+perimeter=2*(length*breadth)
+print("area is;area")
+print("perimeter is;perimeter")
